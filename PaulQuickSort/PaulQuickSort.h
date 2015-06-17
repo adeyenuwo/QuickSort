96f737eab5e31953a048bcb4cb8aed5e9def858e
Paul Ade <adeyenuwo@gmail.com>
2015-06-17 15:30:53 -0400

    Initial Commit

---
+++ PaulQuickSort/PaulQuickSort.h
@@ -0,0 +1,13 @@
+//
+//  PaulQuickSort.h
+//  PaulQuickSort
+//
+//  Created by Paul Ade on 2015-06-17.
+//  Copyright (c) 2015 Paul Ade. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PaulQuickSort : NSObject
+
+@end
---
+++ PaulQuickSort/PaulQuickSort.m
@@ -0,0 +1,13 @@
+//
+//  PaulQuickSort.m
+//  PaulQuickSort
+//
+//  Created by Paul Ade on 2015-06-17.
+//  Copyright (c) 2015 Paul Ade. All rights reserved.
+//
+
+#import "PaulQuickSort.h"
+
+@implementation PaulQuickSort
+
+@end
